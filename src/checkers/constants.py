e1d177eb8b2ab3235acb2a034fca1e754402019e
Daniel Maximo <65917867+danielpmaximo@users.noreply.github.com>
2023-05-05 16:13:17 -0300

    Add files via upload

--- src/checkers/constants.py
+++ src/checkers/constants.py
@@ -1,19 +1,19 @@
 import pygame
 
-width, height = 800, 800
+WIDTH, HEIGHT = 800, 800
 
-rows, cols = 8, 8
+ROWS, COLS = 8, 8
 
-square_size = width//cols
+SQUARE_SIZE = WIDTH//COLS
 
 # rgb
-red = (255, 0, 0)
-white = (255, 255, 255)
-black = (0, 0, 0)
-blue = (0, 0 , 255)
-green = (0, 245, 0)
-grey = (128, 128, 128)
-grey31 = (79, 79, 79)
-navy = (0, 0, 128)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0 , 255)
+GREEN = (0, 245, 0)
+GREY = (128, 128, 128)
+GREY31 = (79, 79, 79)
+NAVY = (0, 0, 128)
 
 CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (44, 25))
