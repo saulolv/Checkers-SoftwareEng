f38c16fcc3697b6c2c29185487658215ab91df92
Daniel Maximo <65917867+danielpmaximo@users.noreply.github.com>
2023-05-05 16:12:40 -0300

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,41 +1,42 @@
 import pygame
-from checkers.constants import width, height, square_size
-from checkers.board import Board
-from checkers.piece import Piece
-
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, BLACK
+from checkers.game import Game
+#from minimax.algorithm import minimax
 
 FPS = 60
 
-WIN = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Jogo de Damas')
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
 
 def get_row_col_from_mouse(pos):
     x, y = pos
-    row = y // square_size
-    col = x // square_size
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
     return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game = Game(WIN)
 
     while run:
         clock.tick(FPS)
 
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+            
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
                 row, col = get_row_col_from_mouse(pos)
-                piece = board.get_piece(row, col)
-                board.move(piece, 4, 3)
-            
-        board.draw(WIN)
-        pygame.display.update()
-        
+                game.select(row, col)
+
+        game.update()
+    
     pygame.quit()
 
 main()
